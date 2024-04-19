54dba8e03284bd59abfd3e7c249b9def27010115
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-19 14:44:05 +0900

    Scaffold에 appbar와 tabbar 두기

---
+++ scrollview_2/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.purple),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Market Kurly"),
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text("컬리추천")),
+              Tab(child: const Text("신상품")),
+              Tab(child: const Text("베스트")),
+            ],
+          ),
+        ),
+        body: HomeBody(),
+      ),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container(
+            height: 300,
+            color: Colors.greenAccent,
+          ),
+        ),
+        SliverPersistentHeader(delegate: CategoryBreadcrumbs(), pinned: true),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 40,
+            (context, index) {
+              return Container(
+                height: 40,
+                color: Color.fromRGBO(
+                    (index * 45) % 255, (index * 70) % 255, (index * 25), 1.0),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CategoryBreadcrumbs extends SliverPersistentHeaderDelegate {
+  const CategoryBreadcrumbs();
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      height: 48,
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        children: [
+          const Text("총 218개", style: TextStyle(color: Colors.black)),
+          const Spacer(),
+          TextButton(
+            onPressed: () {},
+            child: const Center(child: Text("필터")),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => 48;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => 48;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    return false;
+  }
+}
