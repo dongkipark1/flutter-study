40ddd0641f124843fe25284ae3f718b0276fda32
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-04-19 17:29:55 +0900

    당근마켓 완료

--- carrot_app/lib/main.dart
+++ carrot_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:carrot_app/models/chat_message.dart';
 import 'package:carrot_app/models/icon_menu.dart';
 import 'package:carrot_app/models/neighborhood_life.dart';
 import 'package:carrot_app/models/products.dart';
+import 'package:carrot_app/models/recommend_store.dart';
 import 'package:carrot_app/screens/main_screens.dart';
 import 'package:carrot_app/theme.dart';
 import 'package:flutter/cupertino.dart';
@@ -53,4 +54,31 @@ class ImageContainer extends StatelessWidget {
   }
 }
 
+class RoundBorderText extends StatelessWidget {
+  const RoundBorderText({Key? key, required this.title, required this.position})
+      : super(key: key);
+
+  final String title;
+  final int position;
+
+  @override
+  Widget build(BuildContext context) {
+    var paddingValue = position == 0 ? 16.0 : 8.0;
+    return Padding(
+      padding: EdgeInsets.only(left: paddingValue),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18.0),
+          border: Border.all(width: 0.5, color: Colors.grey),
+        ),
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
 
--- carrot_app/lib/screens/near_me/near_me_screen.dart
+++ carrot_app/lib/screens/near_me/near_me_screen.dart
@@ -1,12 +1,106 @@
+import 'package:carrot_app/main.dart';
+import 'package:carrot_app/models/recommend_store.dart';
+import 'package:carrot_app/screens/components/appbar_preferred_size.dart';
+import 'package:carrot_app/screens/near_me/components/bottom_title_icon.dart';
+import 'package:carrot_app/screens/near_me/components/search_text_field.dart';
+import 'package:carrot_app/screens/near_me/components/store_item.dart';
+import 'package:carrot_app/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NearMeScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('nearMeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '내 근처',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.pencil),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.bell),
+          ),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SearchTextField(),
+          ),
+          SizedBox(
+            height: 66,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: searchKeyword.length,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: RoundBorderText(
+                    title: searchKeyword[index],
+                    position: index,
+                  ),
+                );
+              },
+            ),
+          ),
+          Divider(
+            color: Colors.grey,
+            thickness: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 30),
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 22.0,
+              runSpacing: 30,
+              children: [
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.user, title: '구인구직'),
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.edit, title: '과외/클래스'),
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.appleAlt, title: '농수산물'),
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.hotel, title: '부동산'),
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.car, title: '중고차'),
+                const BottomTitleIcon(
+                    iconData: FontAwesomeIcons.chessBishop, title: '전시/행사'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Text('이웃들의 추천 가게', style: textTheme().displayMedium),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: 288,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: recommendStoreList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child: StoreItem(
+                    recommendStore: recommendStoreList[index],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 40),
+        ],
+      ),
     );
   }
 }
